52fef33300fddd3a49a53718c355e6471315a19e
Leober Ramos <leober.ramos33@gmail.com>
2021-03-28 00:50:09 -0400

    feat: add support for last 10 browsers in production

--- src/js/payload.js
+++ src/js/payload.js
@@ -2,44 +2,7 @@ import browser from "webextension-polyfill";
 import { getCurrentTab } from "./utils/tabs";
 import { printMsg, printError } from "./utils/alerts";
 import { getLocaleMsg } from "./utils/localize";
-import GibberishAES from "./vendor/gibberish-aes/gibberish-aes-1.0.0.min";
-
-/**
- * Decrypt a link encrypted with AES and encoded with Base64. Basically a Hackstore.net download link
- *
- * @param {string} link - A string of the browser embed script with the link or a string with the encoded link in Base64.
- *
- * @return {string}
- */
-export function decryptLink(link) {
-    try {
-        const KEY = "fee631d2cffda38a78b96ee6d2dfb43a";
-        const embedScriptRegexp = /link_out = "(\w+)";/;
-        const hackstoreOutRegexp = /https?:\/\/hackstore\.link\/\w+\/?/;
-
-        // Verify if the passed argument is the embed script from the browser, otherwise choose the link passed
-        const encodedLink = embedScriptRegexp.test(link)
-            ? link.match(embedScriptRegexp)[1]
-            : link;
-
-        // Decode the link
-        const decodedLink = atob(encodedLink);
-
-        // Decrypt the link using GibberishAES
-        const decryptedLink = GibberishAES.dec(decodedLink, KEY);
-
-        // If Match the link with the pattern of hackstore out links
-        if (hackstoreOutRegexp.test(decryptedLink)) {
-            // Return it.
-            return decryptedLink.match(hackstoreOutRegexp)[0];
-        }
-
-        // Otherwise return the decrypted link
-        return decryptedLink;
-    } catch (err) {
-        throw new Error(getLocaleMsg("linkDecryptError"));
-    }
-}
+import { decryptLink } from "./utils/decrypt";
 
 /**
  * Execute exploit.
@@ -56,7 +19,12 @@ export default async function execute(tabId) {
                 "(function () { return document.querySelectorAll('script')[2].innerText; })();",
         });
 
-        const link = decryptLink(result);
+        let link;
+        try {
+            link = decryptLink(result);
+        } catch {
+            throw new Error(getLocaleMsg("linkDecryptError"));
+        }
 
         printMsg(
             `${getLocaleMsg(
---
+++ src/js/utils/decrypt.js
@@ -0,0 +1,34 @@
+import GibberishAES from "../vendor/gibberish-aes/gibberish-aes-1.0.0.min";
+
+/**
+ * Decrypt a link encrypted with AES and encoded with Base64. Basically a Hackstore.net download link
+ *
+ * @param {string} link - A string of the browser embed script with the link or a string with the encoded link in Base64.
+ *
+ * @return {string}
+ */
+export function decryptLink(link) {
+    const KEY = "fee631d2cffda38a78b96ee6d2dfb43a"; // Key for AES decryption
+    const embedScriptRegexp = /link_out = "(\w+)";/;
+    const hackstoreOutRegexp = /https?:\/\/hackstore\.link\/\w+\/?/;
+
+    // Verify if the passed argument is the embed script from the browser, otherwise choose the link passed
+    const encodedLink = embedScriptRegexp.test(link)
+        ? link.match(embedScriptRegexp)[1]
+        : link;
+
+    // Decode the link
+    const decodedLink = atob(encodedLink);
+
+    // Decrypt the link using GibberishAES
+    const decryptedLink = GibberishAES.dec(decodedLink, KEY);
+
+    // If Match the link with the pattern of hackstore out links
+    if (hackstoreOutRegexp.test(decryptedLink)) {
+        // Return it.
+        return decryptedLink.match(hackstoreOutRegexp)[0];
+    }
+
+    // Otherwise return the decrypted link
+    return decryptedLink;
+}
